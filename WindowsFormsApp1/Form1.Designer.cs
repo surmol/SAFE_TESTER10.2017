dd89f01f4354a0362b98f836ec8b50017c3a1fe5
Adrian Surma <adrian@rinicom.com>
2017-10-03 22:52:47 +0100

    first push

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.run_test_button = new System.Windows.Forms.Button();
+            this.add_cameras_button = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.current_camera_label = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Camera List";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 72);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(716, 444);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // run_test_button
+            // 
+            this.run_test_button.Location = new System.Drawing.Point(788, 72);
+            this.run_test_button.Name = "run_test_button";
+            this.run_test_button.Size = new System.Drawing.Size(126, 23);
+            this.run_test_button.TabIndex = 3;
+            this.run_test_button.Text = "RUN TEST";
+            this.run_test_button.UseVisualStyleBackColor = true;
+            this.run_test_button.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // add_cameras_button
+            // 
+            this.add_cameras_button.Location = new System.Drawing.Point(788, 142);
+            this.add_cameras_button.Name = "add_cameras_button";
+            this.add_cameras_button.Size = new System.Drawing.Size(117, 23);
+            this.add_cameras_button.TabIndex = 4;
+            this.add_cameras_button.Text = "Add cameras";
+            this.add_cameras_button.UseVisualStyleBackColor = true;
+            this.add_cameras_button.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(860, 116);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(75, 20);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(751, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "How many cameras:";
+            // 
+            // current_camera_label
+            // 
+            this.current_camera_label.AutoSize = true;
+            this.current_camera_label.Location = new System.Drawing.Point(877, 24);
+            this.current_camera_label.Name = "current_camera_label";
+            this.current_camera_label.Size = new System.Drawing.Size(35, 13);
+            this.current_camera_label.TabIndex = 7;
+            this.current_camera_label.Text = "label3";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(794, 251);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(470, 24);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(596, 24);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Change refresh time";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(941, 376);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Add frames";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(794, 376);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            -1304428544,
+            434162106,
+            542,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(791, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1140, 544);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.current_camera_label);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.add_cameras_button);
+            this.Controls.Add(this.run_test_button);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button run_test_button;
+        private System.Windows.Forms.Button add_cameras_button;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label current_camera_label;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,387 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        bool started = false;
+        bool clicked = false;
+        private DataSet ds = new DataSet();
+        private DataTable dt = new DataTable();
+        public int last_serial_number;
+        public int amount_of_cameras;
+        public int time_refresh;
+        public Form1()
+        {
+
+
+            InitializeComponent();
+
+            get_camera_list();
+
+
+        }
+
+
+        private static readonly Random random = new Random();
+        private static readonly object syncLock = new object();
+        public static int RandomNumber(int min, int max)
+        {
+            lock (syncLock)
+            { // synchronize
+                return random.Next(min, max);
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            get_camera_list();
+
+        }
+
+        public void get_camera_list()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.Refresh();
+            try
+            {
+                // PostgeSQL-style connection string
+                string connstring = String.Format("Server=localhost;Port=5432;User Id=postgres;Password=21040911;Database=SAFE;");
+                // Making connection with Npgsql provider
+                NpgsqlConnection conn = new NpgsqlConnection(connstring);
+                conn.Open();
+                // quite complex sql statement
+                string sql = "SELECT * FROM public.\"Cameras\" ORDER BY \"Id\" DESC";
+                // data adapter making request from our connection
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, conn);
+                // i always reset DataSet before i do
+                // something with it.... i don't know why :-)
+                ds.Reset();
+                // filling DataSet with result from NpgsqlDataAdapter
+                da.Fill(ds);
+                // since it C# DataSet can handle multiple tables, we will select first
+                dt = ds.Tables[0];
+                // connect grid to DataTable
+                dataGridView1.DataSource = dt;
+                // since we only showing the result we don't need connection anymore
+                conn.Close();
+            }
+            catch (Exception msg)
+            {
+                // something went wrong, and you wanna know why
+                MessageBox.Show(msg.ToString());
+                throw;
+            }
+        }
+
+        public async void send_camera(string cameraSerialNumber, string timestamp)
+        {
+
+            int pose_random = RandomNumber(-1, 11); // creates a number between 1 and 12
+            int dangerlevel_random = RandomNumber(-1, 5); // creates a number between 1 and 6            
+            var pose = pose_random.ToString();
+            var dangerlevel = dangerlevel_random.ToString();
+            HttpClient httpClient = new HttpClient();
+            MultipartFormDataContent camera = new MultipartFormDataContent();
+
+            camera.Add(new StringContent(cameraSerialNumber), "cameraSerialNumber");
+            camera.Add(new StringContent(timestamp), "timestamp");
+            camera.Add(new StringContent(pose), "pose");
+            camera.Add(new StringContent(dangerlevel), "dangerlevel");
+            // form.Add(new ByteArrayContent(imagebytearraystring, 0, imagebytearraystring.Count()), "profile_pic", "hello1.jpg");
+
+            HttpResponseMessage response = await httpClient.PostAsync("http://192.168.1.56:8089/SubmitFrame", camera);
+
+            response.EnsureSuccessStatusCode();
+            httpClient.Dispose();
+            string sd = response.Content.ReadAsStringAsync().Result;
+        }
+
+        ManualResetEvent CameraPoschange = new ManualResetEvent(false);
+
+        public void send_cameras()
+        {
+
+            for (int i = 0; ; i++)
+            {
+
+                int amount = get_amount_of_cameras();
+
+                DateTime date = DateTime.Now;
+                var unix_time = (long)(date - new DateTime(1970, 1, 1)).TotalMilliseconds;
+                var timestamp = unix_time.ToString();
+
+                for (int n = 1; n <= amount; n++)
+                {
+                    if (clicked)
+                    {
+                        if (IsHandleCreated)
+                            Invoke(new EventHandler(delegate
+                            {
+                                current_camera_label.Text = n.ToString();
+
+                            }));
+
+
+
+
+                        send_camera(n.ToString(), timestamp);
+                        
+                    }
+                    CameraPoschange.WaitOne(time_refresh);
+                }
+
+
+
+
+
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (started && clicked)
+            {
+                clicked = false;
+                run_test_button.Text = "RUN TEST";
+            }
+            else if (!started && !clicked)
+            {
+                Thread x = new Thread(send_cameras);
+                x.Start();
+                started = true;
+                clicked = true;
+                run_test_button.Text = "STOP TEST";
+            }
+            else if (started && !clicked)
+            {
+                clicked = true;
+                run_test_button.Text = "STOP TEST";
+            }
+
+        }
+        public int get_amount_of_cameras()
+        {
+
+            try
+            {
+                // PostgeSQL-style connection string
+                string connstring = String.Format("Server=localhost;Port=5432;User Id=postgres;Password=21040911;Database=SAFE;");
+                // Making connection with Npgsql provider
+                NpgsqlConnection conn = new NpgsqlConnection(connstring);
+                conn.Open();
+                // quite complex sql statement
+                string sql = "SELECT count(*) FROM public.\"Cameras\"";
+
+                NpgsqlCommand command = new NpgsqlCommand(sql, conn);
+                int val;
+                NpgsqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    amount_of_cameras = Int32.Parse(reader[0].ToString());
+
+                }
+                //MessageBox.Show(amount_of_cameras.ToString());
+                conn.Close();
+                return amount_of_cameras;
+            }
+            catch (Exception msg)
+            {
+                // something went wrong, and you wanna know why
+                MessageBox.Show(msg.ToString());
+                throw;
+            }
+
+        }
+        public int get_last_serial_number()
+        {
+
+            try
+            {
+                // PostgeSQL-style connection string
+                string connstring = String.Format("Server=localhost;Port=5432;User Id=postgres;Password=21040911;Database=SAFE;");
+                // Making connection with Npgsql provider
+                NpgsqlConnection conn = new NpgsqlConnection(connstring);
+                conn.Open();
+                // quite complex sql statement
+                string sql = "SELECT \"Id\",\"SerialNumber\" FROM public.\"Cameras\" ORDER BY \"Id\" DESC LIMIT 1";
+
+                NpgsqlCommand command = new NpgsqlCommand(sql, conn);
+                int val;
+                NpgsqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    last_serial_number = Int32.Parse(reader[1].ToString());
+
+                }
+                //MessageBox.Show(last_serial_number.ToString());
+                conn.Close();
+                return last_serial_number;
+            }
+            catch (Exception msg)
+            {
+                // something went wrong, and you wanna know why
+                MessageBox.Show(msg.ToString());
+                throw;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int amount = Convert.ToInt32(numericUpDown1.Value);
+            //get_last_serial_number();
+            add_cameras(amount);
+            get_camera_list();
+        }
+
+        public void add_cameras(int amount)
+        {
+            //int start_serial = get_last_serial_number();
+            int start_serial = 0;
+            for (int i = start_serial + 1; i <= start_serial + amount; i++)
+            {
+
+                try
+                {
+                    int d = 0;
+                    // PostgeSQL-style connection string
+                    string connstring = String.Format("Server=localhost;Port=5432;User Id=postgres;Password=21040911;Database=SAFE;");
+                    NpgsqlConnection conn = new NpgsqlConnection(connstring);
+                    conn.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand("insert into public.\"Cameras\" (\"Description\",\"Name\",\"CameraStatus\",\"SerialNumber\",\"FramesMode\") values(:Description, :Name,:CameraStatus,:SerialNumber,:FramesMode)", conn);
+                    cmd.Parameters.Add(new NpgsqlParameter("Description", "Desc" + i.ToString()));
+                    cmd.Parameters.Add(new NpgsqlParameter("Name", "Cam" + i.ToString()));
+                    cmd.Parameters.Add(new NpgsqlParameter("CameraStatus", 1));
+                    cmd.Parameters.Add(new NpgsqlParameter("SerialNumber", i));
+                    cmd.Parameters.Add(new NpgsqlParameter("FramesMode", d));
+                    cmd.ExecuteNonQuery();
+                    //int Id = Convert.ToInt32(cmd.ExecuteScalar());
+                    //MessageBox.Show(Id.ToString());
+                    conn.Close();
+                }
+                catch (Exception msg)
+                {
+                    // something went wrong, and you wanna know why
+                    MessageBox.Show(msg.ToString());
+                    throw;
+                }
+
+
+            }
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            int x = get_amount_of_cameras();
+        }
+
+
+        private void closeForm(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                this.Invoke(new MethodInvoker(delegate { this.Close(); }));
+
+            }
+            catch { }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            if (IsHandleCreated)
+                Invoke(new EventHandler(delegate
+                {
+                    time_refresh = Convert.ToInt32(numericUpDown2.Value);
+
+                }));
+
+        }
+
+
+
+        public void add_frames(int amount)
+        {
+            if (IsHandleCreated)
+                Invoke(new EventHandler(delegate
+                {
+                   label1.Text = "TEST STARTED !!!";
+
+                }));
+            
+            //int start_serial = get_last_serial_number();
+            int start_serial = 0;
+            for (int i = 0; i <= amount; i++)
+            {
+                for (int g = 73; g <= 82; g++)
+                {
+                    try
+                    {
+
+                        int pose_random = RandomNumber(-1, 11); // creates a number between 1 and 12
+                        int dangerlevel_random = RandomNumber(-1, 5); // creates a number between 1 and 6            
+                        var pose = pose_random.ToString();
+                        var dangerlevel = dangerlevel_random.ToString();
+                        DateTime date = DateTime.Now;
+                        var unix_time = (long)(date - new DateTime(1970, 1, 1)).TotalMilliseconds;
+                        var timestamp = unix_time.ToString();
+                        int d = 0;
+                        // PostgeSQL-style connection string
+                        string connstring = String.Format("Server=localhost;Port=5432;User Id=postgres;Password=21040911;Database=SAFE;");
+                        NpgsqlConnection conn = new NpgsqlConnection(connstring);
+                        conn.Open();
+                        NpgsqlCommand cmd = new NpgsqlCommand("insert into public.\"Frames\" (\"CameraId\",\"DangerLevel\",\"Pose\",\"Timestamp\") values(:CameraId, :DangerLevel,:Pose,:Timestamp)", conn);
+                        cmd.Parameters.Add(new NpgsqlParameter("CameraId", g));
+                        cmd.Parameters.Add(new NpgsqlParameter("DangerLevel", dangerlevel_random));
+                        cmd.Parameters.Add(new NpgsqlParameter("Pose", pose_random));
+                        cmd.Parameters.Add(new NpgsqlParameter("Timestamp",DateTime.Now));
+
+                        cmd.ExecuteNonQuery();
+                        //int Id = Convert.ToInt32(cmd.ExecuteScalar());
+                        //MessageBox.Show(Id.ToString());
+                        conn.Close();
+                    }
+                    catch (Exception msg)
+                    {
+                        // something went wrong, and you wanna know why
+                        MessageBox.Show(msg.ToString());
+                        throw;
+                    }
+                }
+
+
+
+            }
+
+            if (IsHandleCreated)
+                Invoke(new EventHandler(delegate
+                {
+                    label1.Text = "test finished";
+                }));
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int ss = Convert.ToInt32(numericUpDown3.Value);
+            add_frames(ss);
+        }
+    }
+}
